49e328428b3110bc9eb35143b60aff6eba1b9315
Seyed Mohammad Ghaffarian <s.m.ghaffarian@gmail.com>
2018-09-19 19:52:57 +0430

    Initial project commit

---
+++ src/test/java/ghaffarian/nanologger/LoggerTest.java
@@ -0,0 +1,16 @@
+package ghaffarian.nanologger;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for Nano-Logger.
+ */
+public class LoggerTest 
+{
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
